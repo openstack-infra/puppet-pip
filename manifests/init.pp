28bed6d86274830d260a03f0dc4e44bde7ce86c0
Yolanda Robla <yolanda.robla-mota@hp.com>
2015-03-27 13:02:38 +0100

    Add the ability to set pip.conf and configure it
    Create an initial pip.conf file and allow passing
    several settings such as index url and trusted
    hosts.
    
    Change-Id: I78962555c9a9ec1a96ce19810a463a5d619b04f9

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,10 +1,26 @@
 # Class: pip
 #
-class pip {
+class pip (
+  $index_url = 'https://pypi.python.org/simple',
+  $trusted_hosts = [],
+  $manage_pip_conf = false,
+) {
   include pip::params
+  validate_array($trusted_hosts)
 
   package { $::pip::params::python_devel_package:
     ensure => present,
   }
 
+  if $manage_pip_conf != false {
+    if ! defined(File['/etc/pip.conf']) {
+      file { '/etc/pip.conf':
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0444',
+        content => template('pip/pip.conf.erb'),
+        replace => true,
+      }
+    }
+  }
 }
