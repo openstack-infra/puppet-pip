20975e993f3ec30202a5f01aa374d2495ac19d37
Paul Belanger <pabelanger@redhat.com>
2017-05-31 12:26:16 -0400

    Add pip3 support
    We are wanting to start running python3 applications in production, as
    a results we need to ensure that pip3 is created.
    
    Because get-pip.py will override existing pip executables, it will be
    possible for an existing server to have is version change to python3.
    As a result, symlink specifically to pip2 to ensure we have defaulted
    properly to python3.
    
    Change-Id: I8f78dfc62cd3745e900eec573e80236137ac5c78
    Signed-off-by: Paul Belanger <pabelanger@redhat.com>

--- manifests/init.pp
+++ manifests/init.pp
@@ -13,9 +13,26 @@ class pip (
     ensure => present,
   }
 
+  if $::operatingsystem != 'CentOS' {
+      exec { 'download-pip3':
+        command => "/usr/bin/curl ${::pip::params::get_pip_location} | /usr/bin/python3 - -U --force-reinstall",
+        creates => $::pip::params::get_pip3_path,
+        before  => Exec['download-pip'],
+        notify  => File[$::pip::params::get_pip_path]
+    }
+  }
+
   exec { 'download-pip':
-    command => "/usr/bin/curl ${::pip::params::get_pip_location} | /usr/bin/python",
-    creates => $::pip::params::get_pip_path,
+    command => "/usr/bin/curl ${::pip::params::get_pip_location} | /usr/bin/python - -U --force-reinstall",
+    creates => $::pip::params::get_pip2_path,
+    notify  => File[$::pip::params::get_pip_path]
+  }
+
+  # NOTE(pabelanger): Default to pip2 for backwards compat
+  file { $::pip::params::get_pip_path:
+    ensure      => link,
+    refreshonly => true,
+    target      => $::pip::params::get_pip2_path,
   }
 
   if $manage_pip_conf {
