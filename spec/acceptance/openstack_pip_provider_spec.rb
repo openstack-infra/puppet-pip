bfd247c83f0af42865f0371093bae194d29ca5d9
Colleen Murphy <colleen@gazlene.net>
2017-08-22 00:21:44 +0200

    (WIP) Fix the wheel url in functional tests' mirror config
    Change-Id: I1db2edbc87ae7e5957f690bace10ba39772b856a

--- spec/acceptance/openstack_pip_provider_spec.rb
+++ spec/acceptance/openstack_pip_provider_spec.rb
@@ -13,7 +13,6 @@ describe 'custom pip provider' do
 timeout = 60
 index-url = http://mirror.dfw.rax.openstack.org/pypi/simple
 trusted-host = mirror.dfw.rax.openstack.org
-extra-index-url = http://mirror.dfw.rax.openstack.org/wheel/ubuntu-14.04-x86_64
 EOF
       shell("if [ ! -f /etc/pip.conf ] ; then echo '#{pip_conf}' > /etc/pip.conf ; fi")
       # Block pypi.python.org so we know the mirror is working
