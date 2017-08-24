1c6e7f380e5d81f31863279e96ceeb850c8a9e9e
Clark Boylan <clark.boylan@gmail.com>
2017-08-24 15:22:46 -0700

    Trim the test package list to just shade
    We were installing ~24 packages before. Problem with this is it takes
    quite a bit of time and is subject to many external failures. Installing
    just shade should still be a valid test of significant coverage as it is
    a non trivial python package and we use it in quite a few places.
    
    Change-Id: I9cefc83ff280ca00cb434dbe6a1a2357163bc8a6

--- spec/acceptance/fixtures/openstack_pip.pp
+++ spec/acceptance/fixtures/openstack_pip.pp
@@ -1,31 +1,7 @@
 include pip
 
 $packages = [
-  'bandersnatch',
-  'elasticsearch-curator',
-  'gear',
-  'git-review',
-  'irclog2html',
-  'gerritbot',
-  'keyring',
-  'ndg-httpsclient',
-  'pyasn1',
-  'PyGithub',
-  'pyOpenSSL',
-  'python-jenkins',
-  'python-subunit',
-  'python-swiftclient',
-  'PyYAML',
-  'requests',
-  'requestsexceptions',
   'shade',
-  'statsd',
-  'SQLAlchemy',
-  'subunit2sql',
-  'testtools',
-  'tox',
-  'virtualenv',
-  'yappi',
 ]
 package { $packages:
   ensure   => latest,
