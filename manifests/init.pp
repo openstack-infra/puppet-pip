bd054ab0c093b03adf4a8cc8bb26e5abfca9c859
Paul Belanger <pabelanger@redhat.com>
2017-08-24 12:39:48 -0400

    Ensure curl is installed
    Depends-On: I8b346cbfb908c5d2094c5e46ff008ef735b0e903
    Depends-On: Icd0c652ff2f522ce2ea9086965f53e1a03f0d4fd
    Depends-On: I17c119e81a035916550de44356b9f4badd505c81
    Change-Id: Ia8e6bf0a0dd686933cd04a5e678d1f88ebdd8245
    Signed-off-by: Paul Belanger <pabelanger@redhat.com>

--- manifests/init.pp
+++ manifests/init.pp
@@ -12,20 +12,27 @@ class pip (
   package { $::pip::params::python_devel_package:
     ensure => present,
   }
+  if !defined(Package['curl']) {
+    package { 'curl':
+      ensure => present,
+    }
+  }
 
   if $::operatingsystem != 'CentOS' {
       exec { 'download-pip3':
         command => "/usr/bin/curl ${::pip::params::get_pip_location} | /usr/bin/python3 - -U --force-reinstall",
         creates => $::pip::params::get_pip3_path,
         before  => Exec['download-pip'],
-        notify  => Exec[$::pip::params::get_pip_path]
+        notify  => Exec[$::pip::params::get_pip_path],
+        require => Package['curl'],
     }
   }
 
   exec { 'download-pip':
     command => "/usr/bin/curl ${::pip::params::get_pip_location} | /usr/bin/python - -U --force-reinstall",
     creates => $::pip::params::get_pip2_path,
-    notify  => Exec[$::pip::params::get_pip_path]
+    notify  => Exec[$::pip::params::get_pip_path],
+    require => Package['curl'],
   }
 
   # NOTE(pabelanger): Default to pip2 for backwards compat
