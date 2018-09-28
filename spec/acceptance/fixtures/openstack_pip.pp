ee47cb2cde36a3a451565b53a8ca399b5a890318
Colleen Murphy <colleen.murphy@suse.de>
2018-09-28 11:51:03 +0200

    [debug] Fix openstack_pip provider for pip 18
    The output of `pip list --outdated` is different for pip 18. This patch
    adjusts the openstack_pip provider to handle old and new versions of
    pip.
    
    Change-Id: I185116a2701688860fa5ca9f8ef34d6dbecd0174

--- spec/acceptance/fixtures/openstack_pip.pp
+++ spec/acceptance/fixtures/openstack_pip.pp
@@ -1,10 +1,11 @@
 include pip
 
-$packages = [
-  'shade',
-]
-package { $packages:
-  ensure   => latest,
-  provider => openstack_pip,
-  require  => Class['pip'],
-}
+#$packages = [
+#  'shade',
+#  'diskimage-builder',
+#]
+#package { $packages:
+#  ensure   => latest,
+#  provider => openstack_pip,
+#  require  => Class['pip'],
+#}
