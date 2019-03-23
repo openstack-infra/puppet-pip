dc5da0b31f24197fcbad90898082b9192d9134c2
Clark Boylan <clark.boylan@gmail.com>
2019-03-22 17:09:42 -0700

    Use pip list format to find outdated packages
    At some point pip was updated to stop using its legacy default listing
    format and now defaults to columns. Thankfully we can ask older pip to
    list in columns too. Fix our compat with new pip by listing outdated
    packages with an explicit columns format so that we work with new and
    old pip.
    
    Change-Id: Ibcdb4ed83ffc2b77e91d31c0175e9c99d442f07c

--- lib/puppet/provider/package/openstack_pip.rb
+++ lib/puppet/provider/package/openstack_pip.rb
@@ -12,15 +12,20 @@ Puppet::Type.type(:package).provide(:openstack_pip, :parent => :pip) do
   commands :pip => 'pip'
 
   def self.outdated
-    @outdated ||= pip(['list', '--outdated'])
+    @outdated ||= pip(['list', '--outdated', '--format columns'])
   end
 
   def latest
     outdated = self.class.outdated
     if outdated =~ /#{@resource[:name]}/
+      # Output looks like:
+      # Package Version Latest Type
+      # ------- ------- ------ -----
+      # gear    0.12.0  0.13.0 wheel
+      # pip     9.0.1   19.0.3 wheel
       latest = outdated.split("\n").select { |line|
         line =~ /#{@resource[:name]}/
-      }.first.match('Latest: ([^\s)]*)')[1]
+      }.first.match("#{@resource[:name]}\s+[^\s]+\s+([^\s]+)")[1]
     else
       package_info = lazy_pip(['show', @resource[:name]])
       current = package_info.split("\n").select { |line|
